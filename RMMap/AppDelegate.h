976a4de90aaa87f74916bdf87de5bf2865a582ea
rjmankar <rjmankar@yahoo.com>
2016-09-06 17:53:10 +0530

    initial commit

---
+++ RMMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RMMap
+//
+//  Created by Student P_05 on 06/09/16.
+//  Copyright © 2016 Rahul Mankar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RMMap/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  RMMap
+//
+//  Created by Student P_05 on 06/09/16.
+//  Copyright © 2016 Rahul Mankar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface ViewController : UIViewController<MKMapViewDelegate,CLLocationManagerDelegate>
+
+
+@end
+
